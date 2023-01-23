e804916be21339d314c7b3a28ea55bda56cebd19
Nick Johnson <nlj222@gmail.com>
2023-01-23 15:45:06 -0800

    built saveButton functionality

--- index.js
+++ index.js
@@ -24,20 +24,44 @@
 
 const mainbody = document.querySelector('#mainbody')
 
-fetch('./db.json')
+const sidebar = document.querySelector('#sidebar')
+
+const saveButton = document.createElement('button')
+saveButton.setAttribute("type","button")
+saveButton.setAttribute("name","button")
+saveButton.textContent = 'Save Album'
+
+fetch('http://localhost:3000/punk')
     .then(res=>res.json())
     .then(data=> {
-        console.log(data);
-        console.log(data.punk);
-        console.log(data.punk[0]);
-        console.log(data.punk[0].name);
-        const image = document.createElement('img');
-        image.src = data.punk[0].image;
-        const name = document.createElement('h3');
-        name.textContent = data.punk[0].name;
-        const artist = document.createElement('h4');
-        artist.textContent = data.punk[0].artist;
-        const year = document.createElement('h4');
-        year.textContent = data.punk[0].year;
-        mainbody.appendChild(image);
+        let album = data[0];
+        console.log(album);
+        const albumImage = document.createElement('img');
+        albumImage.src = data[0].image;
+        const albumName = document.createElement('h3');
+        albumName.textContent = data[0].name;
+        const albumArtist = document.createElement('h4');
+        albumArtist.textContent = data[0].artist;
+        const albumYear = document.createElement('h4');
+        albumYear.textContent = data[0].year;
+        mainbody.appendChild(albumImage);
+        
+        /////////////
+        mainbody.appendChild(saveButton);
+        const nameAndArtist = document.createElement('p');
+        nameAndArtist.setAttribute('id',album.name.replaceAll(' ',''));
+        nameAndArtist.textContent = `${album.name} by ${album.artist}`
+        saveButton.addEventListener('click', ()=>{
+            if (saveButton.textContent === "Save Album") {
+                sidebar.appendChild(nameAndArtist)
+                saveButton.textContent = "Remove Album"
+            } else if (saveButton.textContent === "Remove Album") {
+                let elementToRemove = document.querySelector(`#${album.name.replaceAll(' ','')}`);
+                // debugger
+                elementToRemove.remove();
+                saveButton.textContent = "Save Album"
+            }
+        })
+
     }) 
+
