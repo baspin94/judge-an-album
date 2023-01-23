0b3082bb24bec7cc32609cd82b02808eb2d4da97
Nick Johnson <nlj222@gmail.com>
2023-01-23 12:36:33 -0800

    added to json file, built out some html and css

--- index.js
+++ index.js
@@ -10,14 +10,23 @@
 //     data.genres.forEach(genre => {console.log(genre.name)});
 // })
 
-fetch('https://musicbrainz.org/ws/2/tag/', {
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
-})
-.then(res=>res.json())
-.then(data=> {
-    console.log(data.genres);
-    data.genres.forEach(genre => {console.log(genre.name)});
-})
+// fetch('https://musicbrainz.org/ws/2/genre/?query="funk"', {
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//     }
+// })
+// .then(res=>res.json())
+// .then(data=> {
+//     console.log(data.genres);
+//     data.genres.forEach(genre => {console.log(genre.name)});
+// })
+
+fetch('./db.json')
+    .then(res=>res.json())
+    .then(data=> {
+        console.log(data);
+        console.log(data.punk);
+        console.log(data.punk[0]);
+        console.log(data.punk[0].name);
+    }) 
