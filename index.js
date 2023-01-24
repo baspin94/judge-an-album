e806bb4de127cc1362fa6c9b65d098c1ba75af04
Ari <aritheriver@gmail.com>
2023-01-24 08:34:29 -0800

    tuesday

--- index.js
+++ index.js
@@ -23,21 +23,41 @@
 // })
 
 const mainbody = document.querySelector('#mainbody')
+const albumBody = document.querySelector('#albumBody')
+        // console.log(data);
+        // console.log(data.punk);
+        // console.log(data.punk[0]);
+        // console.log(data.punk[0].name);
+        // const image = document.createElement('img');
+        // image.src = data.punk[0].image;
+        // const name = document.createElement('h3');
+        // name.textContent = data.punk[0].name;
+        // const artist = document.createElement('h4');
+        // artist.textContent = data.punk[0].artist;
+        // const year = document.createElement('h4');
+        // year.textContent = data.punk[0].year;
+        // mainbody.append(image);
+    //}) 
 
-fetch('./db.json')
+    // function to add eventlistener to drop down to populate the albums
+function renderAlbums(album) {
+    const albumImage = document.createElement('img');
+        albumImage.src = album.image;
+        const albumName = document.createElement('h3');
+        albumName.textContent = album.name;
+        const albumArtist = document.createElement('h4');
+        albumArtist.textContent = album.artist;
+        const albumYear = document.createElement('h4');
+        albumYear.textContent = album.year;
+        albumBody.append(albumImage, albumName, albumArtist, albumYear);
+};
+
+genreSelect = document.querySelector('#genre-list');
+ 
+genreSelect.addEventListener('change', (e) => {
+   albumBody.innerHTML = ' ';
+    const genre = e.target.value.toLowerCase();
+    fetch('http://localhost:3000' + `/${genre}`)
     .then(res=>res.json())
-    .then(data=> {
-        console.log(data);
-        console.log(data.punk);
-        console.log(data.punk[0]);
-        console.log(data.punk[0].name);
-        const image = document.createElement('img');
-        image.src = data.punk[0].image;
-        const name = document.createElement('h3');
-        name.textContent = data.punk[0].name;
-        const artist = document.createElement('h4');
-        artist.textContent = data.punk[0].artist;
-        const year = document.createElement('h4');
-        year.textContent = data.punk[0].year;
-        mainbody.appendChild(image);
-    }) 
+    .then(data=> data.forEach(renderAlbums));
+});
