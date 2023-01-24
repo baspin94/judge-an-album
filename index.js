663022115d171f9537d1195496a8ac3a0bc77645
Nick Johnson <nlj222@gmail.com>
2023-01-24 10:20:46 -0800

    save button fix, it now resets when a new album is selected

--- index.js
+++ index.js
@@ -72,9 +72,13 @@ function renderAlbums(album) {
             albumImage.addEventListener("mouseleave", ()=> {
             //console.log("I'm out!");
             albumDesc.style.display = "none";
-        });
+            });
             singleAlbums.append(bigImage);
             const div = document.createElement('div');
+            const saveButton = document.createElement('button')
+            saveButton.setAttribute("type","button")
+            saveButton.setAttribute("name","button")
+            saveButton.textContent = 'Save Album'
             singleAlbums.appendChild(div);
             div.appendChild(saveButton);
             const nameAndArtist = document.createElement('p');
@@ -107,7 +111,3 @@ genreSelect.addEventListener('change', (e) => {
 });
 const sidebar = document.querySelector('#sidebar')
 
-const saveButton = document.createElement('button')
-saveButton.setAttribute("type","button")
-saveButton.setAttribute("name","button")
-saveButton.textContent = 'Save Album'
