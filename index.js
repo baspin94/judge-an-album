e9df6d4f120d81d84be3560e976b773dc39337b8
Bianca Aspin <bianca@mj94.net>
2023-01-26 08:55:22 -0800

    Clean out old comments.

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ const sidebar = document.querySelector('#sidebar');
 const genreSelect = document.querySelector('#genre-list');
 const singleAlbums = document.querySelector('#singleAlbums');
 const singleAlbumsContainer = document.querySelector('#singleAlbumsContainer')
-// NEW - NICK - define variable for song sample functionality
 const spotifySampler = document.querySelector('iframe');
-
 const bigImage = document.createElement('img');
     bigImage.setAttribute('id','bigImage');
 
-// NEW - Bianca - Fetch and render 'Saved' albums on page load.
+// Fetch and render 'Saved' albums on page load.
 fetch("http://localhost:3000/saved")
     .then(res => res.json())
     .then(data => data.forEach(album => renderSavedAlbum(album)));
 
-// NEW - Bianca - Function to grab name and artist from album object and then add event listener to it.
+// Grab name and artist from album object and then add event listener to it.
 function renderSavedAlbum(album) {
     let button = makeButton(album);
     savedAlbumButton(button, album);
@@ -24,7 +22,7 @@ function renderSavedAlbum(album) {
     nameAndArtistEvent(nameArtist, album, button);
 };
 
-// NEW - Bianca - Function to create a button.
+// Create a button.
 function makeButton(album){
     const saveButton = document.createElement('button')
         saveButtonEvent(album, saveButton);
@@ -32,7 +30,7 @@ function makeButton(album){
     return saveButton;
 };
 
-// NEW - Bianca - Function to add event listener to save button.
+// Add event listener to save button.
 function saveButtonEvent(album, saveButton){
     saveButton.addEventListener('click', ()=>{
         if (saveButton.textContent === "Save Album") {
@@ -61,7 +59,6 @@ function nameAndArtistEvent(nameAndArtist, album, saveButton) {
         singleAlbums.innerHTML = '';
         singleAlbumsContainer.style.display = "block"
         bigImage.src = album.image;
-        //singleAlbums.append(bigImage);
         spotifySampler.src = album.sampleSrc;
         // Grab album name from album object.
         const bigAlbumName = document.createElement('h3');
@@ -78,7 +75,7 @@ function nameAndArtistEvent(nameAndArtist, album, saveButton) {
     })
 };
 
-// NEW - Bianca - Function to grab name and artist from album object.
+// Grab name and artist from album object.
 function nameArtistGrab(album) {
     const nameAndArtist = document.createElement('p');
         nameAndArtist.setAttribute('id', `album_${album.id}`);
@@ -96,8 +93,9 @@ genreSelect.addEventListener('change', (e) => {
         .then(data=> data.forEach(renderAlbums));
     });
 
-// function to add eventlistener to drop down to populate the albums
+// Add eventlistener to drop down to populate the albums
 function renderAlbums(album) {
+
     // Grab album image from album object.
     const albumImage = document.createElement('img');
         albumImage.src = album.image;
@@ -114,11 +112,7 @@ function renderAlbums(album) {
     const albumYear = document.createElement('h4');
         albumYear.textContent = album.year;
 
-    // NEW - Bianca - Removed this.
-    // Grab album id from album object.
-    //const albumId = album.id 
-
-    // NEW - NICK - Grab song sample src from album object.
+    // Grab song sample src from album object.
     const albumSample = album.sampleSrc;
 
     // Create div for each album thumbnail
@@ -130,48 +124,46 @@ function renderAlbums(album) {
         albumDesc.setAttribute("class", "tooltip");
         albumDesc.append(albumName, albumArtist, albumYear);
 
-        // Create event listener to show tooltip when mouse hovers over thumbnail.
-        albumImage.addEventListener("mouseover", () => {
-            albumDesc.style.display = "block"
+    // Create event listener to show tooltip when mouse hovers over thumbnail.
+    albumImage.addEventListener("mouseover", () => {
+        albumDesc.style.display = "block"
 
-            // NEW - NICK - animation
-            albumDesc.animate({
-            width: ['0px', '200px']
-                }, 200
-            );
-            function textAnimate(textElement){
-                textElement.animate({
-                    opacity: ['0', '0', '1'],
-                    offset: ['0', '0.5', '1']
-                }, 350)
-            }
-            [albumName, albumArtist, albumYear].forEach(textAnimate);
-        });
+        // NEW - NICK - animation
+        albumDesc.animate({
+        width: ['0px', '200px']
+            }, 200
+        );
+        function textAnimate(textElement){
+            textElement.animate({
+                opacity: ['0', '0', '1'],
+                offset: ['0', '0.5', '1']
+            }, 350)
+        }
+        [albumName, albumArtist, albumYear].forEach(textAnimate);
+    });
 
-        // Create event listener to hide tooltip when mouse leaves thumbnail.
-        albumImage.addEventListener("mouseleave", () => {
-            // NEW - NICK - animation
-            albumDesc.animate({
-                width: ['200px', '0px']
-                    }, 100
-                ).finished.then(()=>{albumDesc.style.display = "none"}
-            );
-            function textAnimate(textElement){
-                textElement.animate({
-                    opacity: ['1', '0', '0'],
-                    offset: ['0', '0.1', '1']
-                }, 100)
-            }
-            [albumName, albumArtist, albumYear].forEach(textAnimate);
-        });
+    // Create event listener to hide tooltip when mouse leaves thumbnail.
+    albumImage.addEventListener("mouseleave", () => {
+        // NEW - NICK - animation
+        albumDesc.animate({
+            width: ['200px', '0px']
+                }, 100
+            ).finished.then(()=>{albumDesc.style.display = "none"}
+        );
+        function textAnimate(textElement){
+            textElement.animate({
+                opacity: ['1', '0', '0'],
+                offset: ['0', '0.1', '1']
+            }, 100)
+        }
+        [albumName, albumArtist, albumYear].forEach(textAnimate);
+    });
 
     // Add image and tooltip to div and append it to album body.
     albumBody.append(thumbDiv);
     thumbDiv.appendChild(albumImage);
     thumbDiv.appendChild(albumDesc);
     
-    // Set the big image 'src' to be the corresponding album's cover art and append to DOM.
-
     // Grab album name from album object.
     const bigAlbumName = document.createElement('h3');
         bigAlbumName.textContent = album.name;
@@ -197,17 +189,6 @@ function renderAlbums(album) {
         bigImage.src = album.image;
         mainAppend();
 
-        // NEW - Bianca - Removed and replaced with 'Save Button'
-        /* // Defining button to be appended later.
-        const saveButton = document.createElement('button');
-            saveButton.setAttribute("type","button");
-            saveButton.setAttribute("name","button");
-            saveButton.setAttribute("id","saveButton");
-            if (album.post === true) {
-                saveButton.textContent = "Remove Album"
-            } else {
-                saveButton.textContent = 'Save Album';
-            }; */
         let saveButton = makeButton(album);
 
         // NEW - NICK - make save button be a functional remove button if the album was already saved
@@ -231,68 +212,13 @@ function renderAlbums(album) {
         singleAlbums.appendChild(div);
         div.appendChild(saveButton);
         
-        // NEW - NICK - add src to the song sample functionality
+        // Add src to the song sample functionality
         spotifySampler.src = albumSample;
 
-        //let nameArtist = nameArtistGrab(album);
-        //nameAndArtistEvent(nameArtist, album, saveButton);
-        /* // Create paragraph for name and artist to appear in 'Saved Albums'
-        
-        // Create paragraph for name and artist to appear in 'Saved Albums'
-        const nameAndArtist = document.createElement('p');
-        nameAndArtist.setAttribute('id',album.name.replaceAll(' ',''));
-        nameAndArtist.textContent = `"${album.name}" by ${album.artist}` */
-
-        // NEW - Bianca - Moved this into the nameArtistGrab function.
-        /* // NEW - NICK - create event listener on the saved album which will repopulate the big image
-        nameAndArtist.addEventListener('click', ()=>{
-            singleAlbums.innerHTML = '';
-            bigImage.src = album.image;
-            mainAppend();
-            singleAlbums.appendChild(div);
-            div.appendChild(saveButton);
-            spotifySampler.src = albumSample;
-            saveButton.textContent = 'Remove Album';
-        }) */
-
-        // NEW - Bianca - Removed and Added to makeButton function.
-        // Create event listener for 'save' button click.
-        /* saveButton.addEventListener('click', ()=>{
-            if (saveButton.textContent === "Save Album") {
-                saveAlbum(album);
-                saveButton.textContent = "Remove Album";
-            } else if (saveButton.textContent === "Remove Album") {
-                let albumId = saveButton.id.split('_')[1];
-                console.log(banana);
-                removeAlbum(albumId);
-            } */
-            
-            /* // Convert genre name to lowercase to be inserted into URL during fetch.
-            genreCurrent = genreSelect.value.toLowerCase();
-
-            
-
-            
-            // Append the name and artist to 'Saved Albums', update button text, set album 'post' status to true, calls the saveAlbum function.
-            if (saveButton.textContent === "Save Album") {
-                sidebar.appendChild(nameAndArtist)
-                saveButton.textContent = "Remove Album"
-                album.post = true;
-
-            // Remove the name and artist from 'Saved Albums, update button text, set album 'post' status to false, calls the saveAlbum function.
-            } else if (saveButton.textContent === "Remove Album") {
-                let elementToRemove = document.querySelector(`#${album.name.replaceAll(' ','')}`);
-                elementToRemove.remove();
-                saveButton.textContent = "Save Album";
-                album.post = false
-            } */
-            //saveAlbum(album);
-
-        //})
     });
 };
 
-// NEW - Bianca - Initate 'fetch' request to POST album to 'Saved'.
+// Initate 'fetch' request to POST album to 'Saved'.
 function saveAlbum(album, saveButton) {                    
         fetch("http://localhost:3000/saved", {
             method: 'POST',
@@ -308,7 +234,6 @@ function saveAlbum(album, saveButton) {
             })
         })
             .then(response => response.json())
-            //.then(albumData => renderSavedAlbum(albumData));
             .then(albumData => {
                 renderSavedAlbum(albumData);
                 saveButton.textContent = "Remove Album";
@@ -316,9 +241,8 @@ function saveAlbum(album, saveButton) {
             });
 };
 
-// NEW - Bianca - Initiate 'fetch' request to DELETE album from 'Saved'.
+// Initiate 'fetch' request to DELETE album from 'Saved'.
 function removeAlbum(albumId) {
-    //console.log("http://localhost:3000/saved/" + albumId);
     fetch("http://localhost:3000/saved/" + albumId, {
         method: 'DELETE'
     })
